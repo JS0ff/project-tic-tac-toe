7e1f8d8d6f1f07f7327ed9aaa5930d29e8fd627d
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2025-09-26 16:19:34 +0600

    Create new display in the console with user input

--- main.js
+++ main.js
@@ -14,9 +14,26 @@ let secondPlayer = {
 };
 
 // show the game board in the console
-
+// ---note create coordinates for every star to be able
+// to change it when the player moves ---
 let tableDisplay = [];
 for (let i = 0; i < Gameboard().rows; i++) {
   tableDisplay.push([]);
   console.log("{* " + "*" + " *}");
+}
+
+// Ask the player to input the coordinates
+let userX = prompt("Please input the coordinates of X") - 1;
+let userY = prompt("Please input the coordinates of Y") - 1;
+
+for (let i = 0; i < Gameboard().rows; i++) {
+  let row = [];
+  for (let j = 0; j < Gameboard().columns; j++) {
+    if (userX == i && userY == j) {
+      row.push("!");
+    } else {
+      row.push("*");
+    }
+  }
+  console.log(row);
 }
