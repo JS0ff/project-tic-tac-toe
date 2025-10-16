a714451a1a058c5bae56bfa3a200d01668b54a9a
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2025-10-16 13:30:09 +0600

    Create a function that will run one round at a time

--- main.js
+++ main.js
@@ -13,26 +13,27 @@ let secondPlayer = {
   moves: [],
 };
 
-// show the game board in the console
-// ---note create coordinates for every star to be able
-// to change it when the player moves ---
+//Shows the game table
 let tableDisplay = [];
 
-// Ask the player to input the coordinates
-let userX = prompt("Please input the coordinates of X") - 1;
-let userY = prompt("Please input the coordinates of Y") - 1;
+//Make function that runs the game
+function playRound() {
+  // Ask the player to input the coordinates
+  let userX = prompt("Please input the coordinates of X") - 1;
+  let userY = prompt("Please input the coordinates of Y") - 1;
 
-for (let i = 0; i < Gameboard().rows; i++) {
-  let row = [];
-  for (let j = 0; j < Gameboard().columns; j++) {
-    if (userX == i && userY == j) {
-      row.push("!");
-    } else {
-      row.push("*");
+  for (let i = 0; i < Gameboard().rows; i++) {
+    let row = [];
+    for (let j = 0; j < Gameboard().columns; j++) {
+      if (userX == i && userY == j) {
+        row.push("!");
+      } else {
+        row.push("*");
+      }
     }
+    console.log(row);
+    tableDisplay.push(row);
   }
-  console.log(row);
-  tableDisplay.push(row);
 }
 
 // Check if the row or column is complete(diagonal or straight)
