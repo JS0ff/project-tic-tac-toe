b85d7eb1ad55b4d7ac6ca88c2994874d8f5bbe37
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2025-10-17 20:28:49 +0600

    Create a function to save the result of each round

--- main.js
+++ main.js
@@ -16,24 +16,43 @@ let secondPlayer = {
 //Shows the game table
 let tableDisplay = [];
 
-//Make function that runs the game
+//Make function that runs each round of the game
 function playRound() {
-  // Ask the player to input the coordinates
-  let userX = prompt("Please input the coordinates of X") - 1;
-  let userY = prompt("Please input the coordinates of Y") - 1;
+  //If table do not have any values in it, then create blocks
+  if (tableDisplay.length < 3) {
+    // Ask the player to input the coordinates
+    let userX = prompt("Please input the coordinates of X") - 1;
+    let userY = prompt("Please input the coordinates of Y") - 1;
 
-  for (let i = 0; i < Gameboard().rows; i++) {
-    let row = [];
-    for (let j = 0; j < Gameboard().columns; j++) {
-      if (userX == i && userY == j) {
-        row.push("!");
-      } else {
-        row.push("*");
+    for (let i = 0; i < Gameboard().rows; i++) {
+      let row = [];
+      for (let j = 0; j < Gameboard().columns; j++) {
+        if (userX == i && userY == j) {
+          row.push("!");
+        } else {
+          row.push("*");
+        }
       }
+      tableDisplay.push(row);
     }
-    console.log(row);
-    tableDisplay.push(row);
+  } else {
+    userX = prompt("Please input the coordinates of X") - 1;
+    userY = prompt("Please input the coordinates of Y") - 1;
+
+    for (let i = 0; i < Gameboard().rows; i++) {
+      let row = [];
+      for (let j = 0; j < Gameboard().columns; j++) {
+        if (tableDisplay[i][j] === "!") {
+          row.push("!");
+          continue;
+        } else if (userX === i && userY === j) {
+          row.push("!");
+        } else row.push("*");
+      }
+      tableDisplay[i] = row;
+    }
   }
+  return tableDisplay;
 }
 
 // Check if the row or column is complete(diagonal or straight)
