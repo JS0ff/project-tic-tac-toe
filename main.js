b2bfcf4d1c77fa8975b375007ed401a53cfd327c
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2025-11-02 19:47:51 +0600

    Define every board square as cell

--- main.js
+++ main.js
@@ -27,3 +27,18 @@ function Gameboard() {
   };
   return { getBoard, printBoard };
 }
+
+function Cell() {
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
