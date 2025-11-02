a6f00179c9e89f4d61a640f52fbcf1b14f37f20b
Asan Zhorobaev <onlyprogrammingjs@gmail.com>
2025-11-02 18:15:06 +0600

    Create object that will represent the game board

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+/*
+ ** The Gameboard represents the state of the board
+ ** Each square holds a Cell and we expose a
+ ** selectSquare method to be able to add Cells to squares
+ */
+
 function Gameboard() {
   const rows = 3;
   const columns = 3;
   const board = [];
-  return { board, rows, columns };
+
+  // Create 2d array that will represent the state of the game board.
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+  return { getBoard };
 }
